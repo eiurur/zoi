76c847deaf2903f02f3a6cca09450c7ed3676127
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2014-07-22 23:19:08 +0900

    Improved the reading speed of the image

--- public/js/directives.js
+++ public/js/directives.js
@@ -4,76 +4,80 @@
 var items = [
   {
     word: 'がんばる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/huhuh.jpg',
+    image: 'https://pbs.twimg.com/media/BspTawrCEAAwQnP.jpg:large',
     src: 'pic.twitter.com/7hvwgRmZ2r'
   }, {
     word: 'がんばる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/ganbaruzoi.jpg',
+    image: 'https://pbs.twimg.com/media/BspTkipCIAE4a0n.jpg:large',
     src: 'pic.twitter.com/6bdYFIyc3e'
   }, {
     word: 'がんばる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/yosi.jpg',
+    image: 'https://pbs.twimg.com/media/BspWSkvCAAAMi43.jpg:large',
     src: 'pic.twitter.com/VMJbHYLznB'
   }, {
     word: 'がんばる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/owarisouninai.jpg',
+    image: 'https://pbs.twimg.com/media/BspWVoqCEAADtZ4.jpg:large',
     src: 'pic.twitter.com/Y3ht2Ud94J'
   }, {
     word: 'がんばる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/soutai.jpg',
+    image: 'https://pbs.twimg.com/media/BspWaPYCAAAI6Ui.jpg:large',
     src: 'pic.twitter.com/7s5Ke5IQVs'
   }, {
     word: 'がんばる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/ganbbarimasyu.jpg',
+    image: 'https://pbs.twimg.com/media/BswuTdaCQAAQCkg.jpg:large',
     src: 'pic.twitter.com/mrkPJC3DYc'
   }, {
     word: 'あきらめる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/dameda.jpg',
+    image: 'https://pbs.twimg.com/media/BspWc7LCAAAPzhS.jpg:large',
     src: 'pic.twitter.com/DE1dKyLsav'
   }, {
     word: 'あきらめる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/mouyada.jpg',
+    image: 'https://pbs.twimg.com/media/BspWfqoCYAE836J.jpg:large',
     src: 'pic.twitter.com/lkDaoEEC7H'
   }, {
     word: 'かえる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/osakini.jpg',
+    image: 'https://pbs.twimg.com/media/BspWjd0CEAEIzWm.jpg:large',
     src: 'pic.twitter.com/ajwCeJa54A'
   }, {
     word: 'かえる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/otukare.jpg',
+    image: 'https://pbs.twimg.com/media/BswuLr2CMAA1SpE.jpg:large',
     src: 'pic.twitter.com/ltC7iuI7rd'
   }, {
     word: 'ごはんにする',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/gulu.jpg',
+    image: 'https://pbs.twimg.com/media/BspWlZFCMAA4fmV.jpg:large',
     src: 'pic.twitter.com/rWLIjU08mU'
   }, {
     word: 'ごはんにする',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/onakasuita.jpg',
+    image: 'https://pbs.twimg.com/media/BswuMrPCEAEECXg.jpg:large',
     src: 'pic.twitter.com/g0tGY4FtE4'
   }, {
     word: 'ねる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/yasumutoki.jpg',
+    image: 'https://pbs.twimg.com/media/BspWoBQCcAAm9y5.jpg:large',
     src: 'pic.twitter.com/Qa4OOuaSb4'
   }, {
     word: 'わかった',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/wakatta.jpg',
+    image: 'https://pbs.twimg.com/media/BswuH1qCcAAueYw.jpg:large',
     src: 'pic.twitter.com/TzK4f6K8Cf'
   }, {
     word: 'いけるきがする',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/ikeru.jpg',
+    image: 'https://pbs.twimg.com/media/BswuNkICcAE4olR.jpg:large',
     src: 'pic.twitter.com/npWAwmcME6'
   }, {
     word: 'あせる',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/owattenai.png',
+    image: 'https://pbs.twimg.com/media/BswuJviCYAMCdGc.png:large',
     src: 'pic.twitter.com/yh1ObvxjVI'
   }, {
     word: 'しんちょくだめです',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/gogogo.jpg',
+    image: 'https://pbs.twimg.com/media/Bsw1StjCQAA9NQ1.jpg:large',
     src: 'pic.twitter.com/s9R0mikezN'
   }, {
     word: 'きゅうけいにする',
-    image: 'https://dl.dropboxusercontent.com/u/31717228/images/zoi/coffee.jpg',
+    image: 'https://pbs.twimg.com/media/BswuUTPCYAAVX5n.jpg:large',
     src: 'pic.twitter.com/onsbKNL4H7'
+  }, {
+    word: 'おはようございまーす',
+    image: 'https://pbs.twimg.com/media/Bs7qd4uCAAAwalT.jpg:large',
+    src: 'pic.twitter.com/4EWdIZGmuG'
   }
 ];
 angular.module('myApp.directives', [])
@@ -98,22 +102,13 @@ angular.module('myApp.directives', [])
       link: function(scope, element, attrs) {
         var targetPhoto, targetContent, item;
 
-        attrs.suffle = false;
-
         element.bind('click', function() {
           if (!targetPhoto) targetPhoto = document.querySelector('.jumbotron-photo > img');
           if (!targetContent) targetContent = document.querySelector('.jumbotron-contents');
 
-          console.log(targetPhoto);
-          console.log(targetContent);
-
-          if(!attrs.suffle) {
-            item = items[Math.floor(Math.random() * items.length)];
-            targetPhoto.src = item.image;
-            targetContent.innerHTML = '<h1 class="text-center">' + item.word + '<i class="fa fa-chevron-right  word"></i><a href="https://twitter.com/intent/tweet?hashtags=NEWGAME!&url=http://zoi.herokuapp.com&text=' + item.src + '" title="Tweet" target="_blank"><i class="fa fa-twitter"></i> <span class="tweet">tweet</span></a></h1>';
-            // attrs.suffle = !attrs.suffle;  
-            console.log(item.image);
-          }
+          item = items[Math.floor(Math.random() * items.length)];
+          targetPhoto.src = item.image;
+          targetContent.innerHTML = '<h1 class="text-center">' + item.word + '<i class="fa fa-chevron-right  word"></i><a href="https://twitter.com/intent/tweet?hashtags=NEWGAME!&url=http://zoi.herokuapp.com&text=' + item.src + '　\/ " title="Tweet" target="_blank"><i class="fa fa-twitter"></i> <span class="tweet">tweet</span></a></h1>';
         });
       }
     }
@@ -123,7 +118,7 @@ angular.module('myApp.directives', [])
       var item, tag;
 
       item = items[Math.floor(Math.random() * items.length)];
-      tag = '<div class="jumbotron"><div class="jumbotron-photo"><img src="' + item.image + '"></div><div class="jumbotron-contents"><h1 class="text-center">' + item.word + '<i class="fa fa-chevron-right  word"></i><a href="https://twitter.com/intent/tweet?hashtags=NEWGAME!&url=http://zoi.herokuapp.com&text=' + item.src + '" title="Tweet" target="_blank"><i class="fa fa-twitter"></i> <span class="tweet">tweet</span></a></h1></div></div>';
+      tag = '<div class="jumbotron"><div class="jumbotron-photo"><img src="' + item.image + '"></div><div class="jumbotron-contents"><h1 class="text-center">' + item.word + '<i class="fa fa-chevron-right  word"></i><a href="https://twitter.com/intent/tweet?hashtags=NEWGAME!&url=http://zoi.herokuapp.com&text=' + item.src + '　\/ " title="Tweet" target="_blank"><i class="fa fa-twitter"></i> <span class="tweet">tweet</span></a></h1></div></div>';
       elm.append(tag);
     };
   })
