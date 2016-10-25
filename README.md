972cc272d006d0357c451a8939785b9d64b4eaeb
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-10-25 23:01:00 +0900

    rm -rf

--- README.md
+++
@@ -1,9 +0,0 @@
-# zoi
-
-![zoi](https://dl.dropboxusercontent.com/u/31717228/images/zoi/ganbaruzoi.png "zoi")
-
-### <a href="http://zoi.herokuapp.com" target="_blank">zoi - heroku</a>
-
-# License
-
-MIT
--- public/js/controllers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myApp.controllers', [])
-  .controller('AppCtrl', ['$scope', 'NewGameService', function ($scope, NewGameService) {
-    $scope.click = function() {
-        $scope.isOpened = !$scope.isOpened;
-    }
-    $scope.items = NewGameService.items;
-  }]);
